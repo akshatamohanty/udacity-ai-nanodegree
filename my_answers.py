5fd89c5a8187008ca6610e270cbb8a66c19c1d03
Ubuntu <ubuntu@ip-172-31-31-231.ap-northeast-2.compute.internal>
2018-03-09 18:33:58 +0000

    question 1-3

--- my_answers.py
+++ my_answers.py
@@ -18,18 +18,32 @@ def window_transform_series(series, window_size):
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
+    
+    ## numpy strides to extract X
+    nrows = len(series) - window_size
+    n = series.strides[0]
+    X = np.lib.stride_tricks.as_strided(series, shape=(nrows, window_size), strides=(n,n) )
+    
+    ## get all observations starting window_size + 1
+    y = np.reshape(series[window_size:], (nrows, 1))
 
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(window_size):
-    pass
+def build_part1_RNN(window_size, b_inp_shape):
+    hidden_units = 5
+    model = Sequential()
+    model.add(LSTM(hidden_units, batch_input_shape=b_inp_shape))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    special_characters = ['é', 'è', '*','%', '@', '/', 'â', 'à', '$']
+    str_without_punc = text.replace("".join(punctuation), "")
+    cleaned_text = str_without_punc.replace("".join(special_characters), "")
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
