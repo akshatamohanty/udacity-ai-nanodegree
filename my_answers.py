b277d598ea4a4c4ae0ca8983787d79d475057bc7
Ubuntu <ubuntu@ip-172-31-31-231.ap-northeast-2.compute.internal>
2018-03-11 10:48:38 +0000

    question 4

--- my_answers.py
+++ my_answers.py
@@ -15,7 +15,7 @@ def window_transform_series(series, window_size):
 
     # reshape each 
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
+    X.shape = (np.shape(X)[0:window_size])
     y = np.asarray(y)
     y.shape = (len(y),1)
     
@@ -30,7 +30,8 @@ def window_transform_series(series, window_size):
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(window_size, b_inp_shape):
+def build_part1_RNN(window_size):
+    b_inp_shape = (None, X_train.shape[1], X_train.shape[2])
     hidden_units = 5
     model = Sequential()
     model.add(LSTM(hidden_units, batch_input_shape=b_inp_shape))
@@ -41,7 +42,7 @@ def build_part1_RNN(window_size, b_inp_shape):
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-    special_characters = ['é', 'è', '*','%', '@', '/', 'â', 'à', '$']
+    special_characters = ['é', 'è', '*','%', '@', '/', 'â', 'à', '$', '<']
     str_without_punc = text.replace("".join(punctuation), "")
     cleaned_text = str_without_punc.replace("".join(special_characters), "")
     return text
@@ -51,10 +52,44 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    X = []
+    y = []
+    text_arr = np.array(list(text))
 
+    # reshape each 
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:window_size])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+    
+    ## numpy strides to extract X
+    nrows = len(text_arr) - window_size
+    n = text_arr.strides[0]
+    X = np.lib.stride_tricks.as_strided(text_arr, shape=(nrows, window_size), strides=(n,n) )
+    
+    ## get all observations starting window_size + 1
+    y = np.reshape(text_arr[window_size:], (nrows, 1))
+    
+    ## take all inputs every step_size 
+    X = X[::step_size]
+    y = y[::step_size]
+    
+    inputs = X.tolist()
+    outputs = y.tolist()
+    
+    inputs = [''.join(x) for x in inputs]
+    outputs = [''.join(x) for x in outputs]
+    
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    hidden_units = 200
+    unique_chars = num_chars
+    inp_sh = (window_size, unique_chars)
+    model = Sequential()
+    model.add(LSTM(hidden_units, input_shape=inp_sh))
+    model.add(Dense(unique_chars, activation='softmax'))
+    return model
